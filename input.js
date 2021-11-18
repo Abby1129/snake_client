56d7bbd673576ea55ebb48416afa352cf3344c76
Abby <abby_faigao@yahoo.com>
2021-11-18 00:45:57 +0000

    refactored and made an input file

---
+++ input.js
@@ -0,0 +1,20 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", (key) => {
+    handleUserInput(key);
+  });
+  const handleUserInput = function (key) {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  };
+
+  return stdin;
+};
+
+// setupInput();
+
+module.exports = setupInput;
